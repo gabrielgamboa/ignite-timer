bf90136cbdbbdec9c5fdfbaa83373a8fb51b9d3f
Gabriel Gamboa <gabriel.gamboa@mblabs.com.br>
2023-02-06 14:37:17 -0300

    feat: use react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Button'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
+import { Router } from './Router'
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary"></Button>
-      <Button variant="secondary"></Button>
-      <Button variant="danger"></Button>
-      <Button variant="success"></Button>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
--- src/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-import { ButtonContainer } from './Button.styles'
-
-interface ButtonProps {
-  variant: string
-}
-
-export function Button({ variant }: ButtonProps) {
-  return <ButtonContainer variant={variant}>Enviar</ButtonContainer>
-}
