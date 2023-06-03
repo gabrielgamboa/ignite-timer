1637da16fab6eebb738a5afb5ec4f93b43a65f97
Gabriel Gamboa <gabriel.gamboa@mblabs.com.br>
2023-06-03 09:06:17 -0300

    feat: history table

--- src/pages/History/styles.ts
+++ src/pages/History/styles.ts
@@ -1,5 +1,38 @@
 import styled from "styled-components";
 
-export const HistoryContainer = styled.main``;
+export const HistoryContainer = styled.main`
+  flex: 1;
+  padding: 3.5rem;
 
-export const HistoryList = styled.div``;
+  display: flex;
+  flex-direction: column;
+
+  h1 {
+    font-size: 1.5rem;
+    color: ${(props) => props.theme["gray-100"]};
+  }
+`;
+
+export const HistoryList = styled.div`
+  flex: 1;
+  margin-top: 2rem;
+
+  table {
+    width: 100%;
+    border-collapse: collapse;
+
+    th {
+      background: ${(props) => props.theme["gray-600"]};
+      padding: 1rem 1.5rem;
+      text-align: left;
+
+      &:first-child {
+        border-top-left-radius: 8px;
+      }
+
+      &:last-child {
+        border-top-right-radius: 8px;
+      }
+    }
+  }
+`;
